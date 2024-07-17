2c19833fac7ea70ec5682f8bdc4b51ba0383dfc0
Swedz <me@swedz.net>
2024-07-17 04:48:03 -0400

    Move blueprint learning to use item call rather than onItemUseFirst

--- src/main/java/net/swedz/mi_tweaks/items/MachineBlueprintItem.java
+++ src/main/java/net/swedz/mi_tweaks/items/MachineBlueprintItem.java
@@ -15,6 +15,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
@@ -22,11 +23,8 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
-import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.HitResult;
 import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
 import net.swedz.mi_tweaks.MITweaks;
 import net.swedz.mi_tweaks.MITweaksConfig;
@@ -117,6 +115,35 @@ public final class MachineBlueprintItem extends Item
 	}
 	
 	@Override
+	public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand)
+	{
+		ItemStack stack = player.getItemInHand(usedHand);
+		Optional<Block> machineBlockOptional = getMachineBlock(stack);
+		if(machineBlockOptional.isPresent())
+		{
+			Block machineBlock = machineBlockOptional.get();
+			
+			BlueprintsLearned blueprintsLearned = player.getData(MITweaksOtherRegistries.BLUEPRINTS_LEARNED);
+			if(!blueprintsLearned.hasLearned(machineBlock))
+			{
+				if(!level.isClientSide)
+				{
+					blueprintsLearned.learn(machineBlock);
+					new UpdateBlueprintsLearnedPacket(blueprintsLearned).sendToClient((ServerPlayer) player);
+					player.displayClientMessage(MITweaksText.BLUEPRINT_LEARNED.text(ITEM_PARSER.parse(machineBlock.asItem())).withStyle(ChatFormatting.GREEN), true);
+					player.swing(usedHand, true);
+				}
+				return InteractionResultHolder.consume(stack);
+			}
+			else
+			{
+				return InteractionResultHolder.fail(stack);
+			}
+		}
+		return InteractionResultHolder.pass(stack);
+	}
+	
+	@Override
 	public InteractionResult onItemUseFirst(ItemStack stack, UseOnContext context)
 	{
 		Player player = context.getPlayer();
@@ -127,10 +154,8 @@ public final class MachineBlueprintItem extends Item
 		{
 			Block machineBlock = machineBlockOptional.get();
 			
-			BlockHitResult blockHitResult = getPlayerPOVHitResult(level, player, ClipContext.Fluid.NONE);
-			if(blockHitResult.getType() == HitResult.Type.BLOCK &&
-			   level.getBlockState(blockHitResult.getBlockPos()).is(machineBlock) &&
-			   level.getBlockEntity(blockHitResult.getBlockPos()) instanceof MultiblockMachineBlockEntity multiblockMachine &&
+			if(level.getBlockState(context.getClickedPos()).is(machineBlock) &&
+			   level.getBlockEntity(context.getClickedPos()) instanceof MultiblockMachineBlockEntity multiblockMachine &&
 			   !multiblockMachine.isShapeValid())
 			{
 				ShapeMatcher matcher = new ShapeMatcher(
@@ -164,19 +189,6 @@ public final class MachineBlueprintItem extends Item
 						}
 					}
 				}
-			}
-			
-			BlueprintsLearned blueprintsLearned = player.getData(MITweaksOtherRegistries.BLUEPRINTS_LEARNED);
-			if(!blueprintsLearned.hasLearned(machineBlock))
-			{
-				if(!level.isClientSide)
-				{
-					blueprintsLearned.learn(machineBlock);
-					new UpdateBlueprintsLearnedPacket(blueprintsLearned).sendToClient((ServerPlayer) player);
-					player.displayClientMessage(MITweaksText.BLUEPRINT_LEARNED.text(ITEM_PARSER.parse(machineBlock.asItem())).withStyle(ChatFormatting.GREEN), true);
-					player.swing(usedHand, true);
-				}
-				return InteractionResult.CONSUME;
 			}
 		}
 		return InteractionResult.PASS;
