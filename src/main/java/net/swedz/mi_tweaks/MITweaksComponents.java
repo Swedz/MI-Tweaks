d983ad71e935c6a7eec557521917d4217eb15f34
Swedz <me@swedz.net>
2025-01-11 18:15:39 -0800

    Clean up codec internals (#24)

--- src/main/java/net/swedz/mi_tweaks/MITweaksComponents.java
+++ src/main/java/net/swedz/mi_tweaks/MITweaksComponents.java
@@ -1,42 +1,22 @@
 package net.swedz.mi_tweaks;
 
-import com.mojang.serialization.Codec;
-import com.mojang.serialization.DataResult;
-import net.minecraft.ResourceLocationException;
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
+import net.swedz.tesseract.neoforge.helper.CodecHelper;
 
 import java.util.function.Supplier;
 
 public final class MITweaksComponents
 {
-	private static final DeferredRegister.DataComponents COMPONENTS = DeferredRegister.createDataComponents(MITweaks.ID);
+	private static final DeferredRegister.DataComponents COMPONENTS = DeferredRegister.createDataComponents(Registries.DATA_COMPONENT_TYPE, MITweaks.ID);
 	
 	public static final Supplier<DataComponentType<Block>> MACHINE_BLOCK = COMPONENTS.registerComponentType(
 			"machine",
-			(b) -> b.persistent(Codec.STRING.comapFlatMap(
-							(blockId) ->
-							{
-								ResourceLocation resourceLocation;
-								try
-								{
-									resourceLocation = ResourceLocation.parse(blockId);
-								}
-								catch (ResourceLocationException ex)
-								{
-									return DataResult.error(() -> "Not a valid resource location: %s %s".formatted(blockId, ex.getMessage()));
-								}
-								return BuiltInRegistries.BLOCK.getOptional(resourceLocation)
-										.map(DataResult::success)
-										.orElseGet(() -> DataResult.error(() -> "Not a valid block id: %s".formatted(blockId)));
-							},
-							(block) -> BuiltInRegistries.BLOCK.getKey(block).toString()
-					).stable()
-			)
+			(b) -> b.persistent(CodecHelper.forRegistry(BuiltInRegistries.BLOCK))
 	);
 	
 	public static void init(IEventBus bus)
